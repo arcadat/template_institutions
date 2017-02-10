d6f016dd237892c43f488cbb705e4db349ead870
Evers Rivero <evers.rivero@gmail.com>
2017-02-10 13:02:13 -0500

    Agregadas nuevas rutas para: 1-Home permite recibir mediante el método post. 2-Agregada ruta colegio para imprimir el formulario de ingreso de id del colegio. 3-La ruta partials ahora permite recibir el parámetro que indica qué vista se va a mostrar.

--- app/route/home_route.php
+++ app/route/home_route.php
@@ -2,6 +2,10 @@
 
 $app->get('/', 'App\Controller\HomeController:home');
 
+$app->post('/', 'App\Controller\HomeController:home');
+
+$app->get('/colegio', 'App\Controller\HomeController:colegio');
+
 $app->post('/signin', 'App\Controller\HomeController:signin');
 
-$app->post('/partials/user', 'App\Controller\HomeController:user');
+$app->post('/partials/{workers}', 'App\Controller\HomeController:partials');
