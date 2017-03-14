6d656ad3035936c768f055386b033ba789f61fc0
Evers Rivero <evers.rivero@gmail.com>
2017-03-14 01:58:01 -0500

    1- Optimizado el envío de variables a la vista en el método home. 2- Incorporada la utilización del token JWT. 3- Corregido bug que impedía el correcto funcionamiento al ingresar un id de colegio diferente.

--- app/controller/home_controller.php
+++ app/controller/home_controller.php
@@ -19,32 +19,33 @@ class HomeController extends Controller
         if ($req->isPost()) {
             $data = $req->getParsedBody();
             $data = $data['id_col'];
-            $_SESSION['idco'] = $data;
         } else {
             $data = (isset($_SESSION['idco'])) ? $_SESSION['idco'] : $this->container->config->api->colegioId;
         }
+
+        $_SESSION['idco'] = $data;
 
         $uri = $this->container->config->api->url_basic . $data;
 
         $response = Request::get($uri)->send();
 
         if ($response->meta_data['http_code'] == 200) {
-            $main_data         = $response->body->main_data;
-            $covers            = $response->body->covers;
-            $installations     = $response->body->installations;
-            $degree            = $response->body->degree;
-            $schedule_academic = $response->body->schedule_academic;
-            if ($covers->covers) {
-                $photos_covers = $covers->photos_covers;
+            $args['main_data']         = $response->body->main_data;
+            $args['covers']            = $response->body->covers;
+            $args['installations']     = $response->body->installations;
+            $args['degree']            = $response->body->degree;
+            $args['schedule_academic'] = $response->body->schedule_academic;
+            if ($args['covers']->covers) {
+                $args['photos_covers'] = $args['covers']->photos_covers;
             }
-            if ($installations->installations) {
-                $photos_installations = $installations->photos_installations;
+            if ($args['installations']->installations) {
+                $args['photos_installations'] = $args['installations']->photos_installations;
             }
-            if ($degree->degree) {
-                $data_degree = $degree->data_degree;
+            if ($args['degree']->degree) {
+                $args['data_degree'] = $args['degree']->data_degree;
             }
-            if ($schedule_academic->schedule) {
-                foreach ($schedule_academic->data_schedule as $data) {
+            if ($args['schedule_academic']->schedule) {
+                foreach ($args['schedule_academic']->data_schedule as $data) {
                     $start = new DateTime($data->init_schedule->date);
                     $end   = new DateTime($data->end_schedule->date);
                     $end->modify('+1 day');
@@ -59,21 +60,9 @@ class HomeController extends Controller
             } else {
                 $array_data = [];
             }
-            $data_schedule  = json_encode($array_data);
-            $tiempoServicio = UtilityLib::tiempoTranscurrido($main_data->date_foundation->date);
-
-            $args = [
-                'main_data'            => $main_data,
-                'covers'               => $covers,
-                'photos_covers'        => $photos_covers,
-                'installations'        => $installations,
-                'photos_installations' => $photos_installations,
-                'degree'               => $degree,
-                'data_degree'          => $data_degree,
-                'data_schedule'        => $data_schedule,
-                'tiempoServicio'       => $tiempoServicio,
-                'recaptcha_sitekey'    => $this->container->config->api_recaptcha->sitekey,
-            ];
+            $args['data_schedule']  = json_encode($array_data);
+            $args['tiempoServicio'] = UtilityLib::tiempoTranscurrido($args['main_data']->date_foundation->date);
+            $args['recaptcha_sitekey'] = $this->container->config->api_recaptcha->sitekey;
 
             $file = 'home/index.phtml';
         } else {
@@ -103,7 +92,6 @@ class HomeController extends Controller
         $this->container->logger->info("Arcadat Template '/signin' route");
 
         $data = $req->getParsedBody();
-        $data['idco'] = (isset($_SESSION['idco'])) ? $_SESSION['idco'] : $this->container->config->api->colegioId;
 
         if ($data['tn'] >= 3) {
             $recaptcha = new \ReCaptcha\ReCaptcha($this->container->config->api_recaptcha->secret);
@@ -120,19 +108,17 @@ class HomeController extends Controller
         }
 
         if (isset($_SESSION['user'])) {
-            $data['idco']  = $_SESSION['idco'];
             $data['user']  = $_SESSION['user'];
             $data['pass']  = $_SESSION['pass'];
             $data['check'] = $_SESSION['check'];
         } elseif ($data['check']=='true') {
-            $_SESSION['idco']  = $data['idco'];
             $_SESSION['user']  = $data['user'];
             $_SESSION['pass']  = $data['pass'];
             $_SESSION['check'] = $data['check'];
         }
 
         $data = [
-            'i_i' => $data['idco'],
+            'i_i' => $_SESSION['idco'],
             'u'   => $data['user'],
             'p'   => $data['pass'],
             'ncs' => $data['check']
@@ -147,6 +133,12 @@ class HomeController extends Controller
             ->sendsType(Mime::FORM)
             ->body($data)
             ->send();
+
+        if ($data['ncs']=='true') {
+            $data = json_decode($response);
+            $_SESSION['token'] = $data->settings->token;
+            $_SESSION['idper'] = $data->person->id_person;
+        }
 
         // Render Json view
         return $res->withHeader('Content-type', 'application/json')->write($response);
@@ -221,9 +213,8 @@ class HomeController extends Controller
         $resp = $recaptcha->verify($data['recaptcha'], $_SERVER['REMOTE_ADDR']);
 
         if ($resp->isSuccess()) {
-            $data['idco'] = (isset($_SESSION['idco'])) ? $_SESSION['idco'] : $this->container->config->api->colegioId;
             $data = [
-                'i_i'     => $data['idco'],
+                'i_i'     => $_SESSION['idco'],
                 'name'    => $data['name'],
                 'email'   => $data['email'],
                 'subject' => $data['subject'],
@@ -288,9 +279,8 @@ class HomeController extends Controller
         $resp = $recaptcha->verify($data['recaptcha'], $_SERVER['REMOTE_ADDR']);
 
         if ($resp->isSuccess()) {
-            $data['idco'] = (isset($_SESSION['idco'])) ? $_SESSION['idco'] : $this->container->config->api->colegioId;
             $data = [
-                'i_i'     => $data['idco'],
+                'i_i'    => $_SESSION['idco'],
                 'id'     => $data['id'],
                 'db'     => $data['date'],
                 'option' => $data['op'],
@@ -325,9 +315,8 @@ class HomeController extends Controller
 
         $data = $req->getParsedBody();
 
-        $data['idco'] = (isset($_SESSION['idco'])) ? $_SESSION['idco'] : $this->container->config->api->colegioId;
         $data = [
-            'i_i'       => $data['idco'],
+            'i_i'       => $_SESSION['idco'],
             'id_person' => $data['id'],
             'em'        => $data['email'],
             'option'    => $data['op'],
